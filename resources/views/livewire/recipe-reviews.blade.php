f53d6654b08f1d7ec6261517e83c1ae3c7b11934
Mr-ngollo <jacobbullugu72@gmail.com>
2025-01-31 03:40:17 -0800

    updates

--- resources/views/livewire/recipe-reviews.blade.php
+++ resources/views/livewire/recipe-reviews.blade.php
@@ -61,11 +61,12 @@
                                 d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                         </svg>
                     </div>
-                    <div class="flex justify-between items-center">
-                        <span class="font-semibold text-lg text-gray-900">{{ $review->user->name }}</span>
+                    <span class="font-semibold text-lg text-gray-900">{{ $review->user->name }}</span>
+                </div>
+                    <div class="flex justify-end items-center mt-1">
                         <span class="text-sm text-gray-500">{{ $review->created_at->diffForHumans() }}</span>
                     </div>
-                </div>
+                
                 <p class="text-gray-700 mt-2 text-lg">{{ $review->review }}</p>
 
                 <div class="text-yellow-500 text-xl mt-2">
